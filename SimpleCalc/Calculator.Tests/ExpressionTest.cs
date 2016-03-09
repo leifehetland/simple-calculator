b39a64d8d778056ffe4aa3dfcbeedc61fc0fdef2
Leif Hetland <lehetland1@gmail.com>
2016-03-09 15:09:25 -0600

    chunk 1 mostly complete 6 tests passing

--- SimpleCalc/Calculator.Tests/ExpressionTest.cs
+++ SimpleCalc/Calculator.Tests/ExpressionTest.cs
@@ -32,5 +32,68 @@ namespace Calculator.Tests
             
         }
 
+        [TestMethod]
+        public void ExpressionCanIRemoveSpaces()
+        {
+            //Arrange
+            Expression CalcExpression = new Expression();
+
+            //Act
+            var expected = new int[] { 2, 3 };
+            var userinput = "2              +          3";
+            var actual = CalcExpression.GetTwoNumbers(userinput);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void ExpressionCanIHandleLetters()
+        {
+            //Arrange
+            Expression calcExpression = new Expression();
+
+            //Act
+            string badInput = "a + 5";
+            calcExpression.GetTwoNumbers(badInput);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CanIHandleOnlyOneNumber()
+        {
+            //Arrange
+            Expression calcExpression = new Expression();
+
+            //Act
+            string badInput = "1";
+            calcExpression.GetTwoNumbers(badInput);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CanIHandleOnlyMoreThanTwoNumbers()
+        {
+            //Arrange
+            Expression calcExpression = new Expression();
+
+            //Act
+            string badInput = "1 + 3 + 4";
+            calcExpression.GetTwoNumbers(badInput);
+        }
+
+        [TestMethod]
+        public void CanIExtractAnOperator()
+        {
+            //Arrange
+            Expression calcExpression = new Expression();
+
+            //Act
+            string actual = calcExpression.GetOperator("2 + 3");
+
+        }
+
+
     }
 }
---
+++ SimpleCalc/SimpleCalc/Expression.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    public class Expression
+    {
+        public int[] GetTwoNumbers(string expression)
+        {
+            char[] operators = { '+', '-', '/', '*', '%' };
+            string [] onlyInts = expression.Split(operators);
+            List<int> myNumbers = new List<int>();
+
+            foreach (var stringInt in onlyInts)
+            {
+                int number = Int32.Parse(stringInt);
+                myNumbers.Add(number);
+            }
+
+            if (myNumbers.Count != 2)
+            {
+                throw new ArgumentException();
+            }
+
+            return myNumbers.ToArray();
+            
+
+
+        }
+
+        public string GetOperator(string equation)
+        {
+            if (equation.Contains("+"))
+            {
+                return "+";
+            }
+
+            if (equation.Contains("-"))
+            {
+                return "-";
+            }
+
+        }
+    }
+}
